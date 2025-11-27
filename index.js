3f057f5f2f247cb2301b3a0d2f8d0b367bc5178c
AJ-Tan <aldomin22@gmail.com>
2025-11-27 23:07:52 +0800

    Revised code in index.js to separate the players in gameBoard.

--- index.js
+++ index.js
@@ -1,54 +1,68 @@
-const gameBoard = ((player1, player2) => {
-  const players = { player1, player2 }
+const gamePlayers = (() => {
+  const players = {
+    player1: {
+      name: 'Player 1',
+      symbol: 'X'
+    },
+    player2: {
+      name: 'Player 2',
+      symbol: 'O'
+    }
+  }
+
+  return players;
+})();
+
+const gameBoard = (() => {
   const boardData = {
     player1: [],
     player2: []
   };
-  
-  const getPlayers = () => ({...players});
+  const winConditions = [
+    [1,2,3],[1,4,7],[1,5,9],[2,5,8],[3,6,9],
+    [3,5,7]
+  ];
   
   const getBoardData = () => ({...boardData});
   const setBoardData = (player, blockNumber) => {
-    boardData[player] = [...boardData[player], blockNumber];
+    boardData[player] = [...boardData[player], +blockNumber];
+    boardData[player].sort((a, b) => a - b);
   }
   const validateBoardData = (blockNumber) => {
-    if([...boardData.player1, ...boardData.player2].includes(blockNumber)) {
+    if([...boardData.player1, ...boardData.player2].includes(+blockNumber)) {
       return false;
     };
     return true;
   }
 
-  return {getPlayers, getBoardData, setBoardData, validateBoardData};
-})('player1', 'player2');
-
-const gameState = (() => {
-  const {setBoardData, validateBoardData} = gameBoard;
-  let playerTurn = 'player1';
-
-  const togglePlayer = () => {
-    playerTurn = playerTurn === 'player1' ? 'player2' : 'player1';
-  }
-
   const playerInput = (blockNumber) => {
+    const {getPlayerTurn, togglePlayer} = gameState;
+
     if(validateBoardData(blockNumber)) {
-      setBoardData(playerTurn, blockNumber);
+      setBoardData(getPlayerTurn(), blockNumber);
       togglePlayer();
       displayController();
     }
   }
 
-  return {playerInput}
+  return {getBoardData, playerInput};
+})();
+
+const gameState = (() => {
+  let playerTurn = 'player1';
+
+  const getPlayerTurn = () => playerTurn;
+  const togglePlayer = () => {
+    playerTurn = playerTurn === 'player1' ? 'player2' : 'player1';
+  }
+
+  return {getPlayerTurn, togglePlayer}
 })();
 
 const displayController = (() => {
-  const playerSymbols = {
-    player1: 'X',
-    player2: 'O'
-  }
-
   const displaySymbol = function (boardPosition, player) {
     const gameBlockNode = document.querySelector(`.game-block[data-position='${boardPosition}']`);
-    gameBlockNode.textContent = playerSymbols[player];
+    gameBlockNode.textContent = gamePlayers[player].symbol;
   }
 
   const displayBoard = () => {
@@ -63,9 +77,9 @@ const displayController = (() => {
 })();
 
 const gameBlockNode = document.querySelectorAll('button.game-block');
-
 gameBlockNode.forEach(node => {
   node.addEventListener('click', (e) => {
-    gameState.playerInput(e.currentTarget.dataset['position']);
+    gameBoard.playerInput(e.currentTarget.dataset['position']);
+    console.log(gameBoard.getBoardData());
   })
 })
